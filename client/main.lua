9fb4fb2e0ed88d9668cadc4b6de89660840853fd
rex2630 <rex2630@gmail.com>
2020-01-17 22:59:48 +0100

    Minor cleanup

--- client/main.lua
+++ client/main.lua
@@ -1,22 +1,7 @@
-local Keys = {
-	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
-	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
-	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
-	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
+ESX = nil
 
-ESX                             = nil
-
-local GUI                       = {}
-GUI.Time                        = 0
-
-
-
+local GUI = {}
+GUI.Time = 0
 
 local options = {
  x = 0.900,
@@ -44,9 +29,6 @@ function DemarchesMenu()
     Menu.addButton("~o~Quitter", "quitter", nil)
 end
 
-
-
-
 function demHumeurs()
     options.menu_subtitle = "Humeurs"
     ClearMenu()
@@ -64,9 +46,6 @@ function demHumeurs()
 							Menu.addButton("~r~Reset", "reset", nil)
 end
 
-
-
-
 							function humeurs1()
 								local pid = PlayerPedId()
 	
@@ -74,10 +53,7 @@ end
 								RequestAnimSet("move_m@brave")
 								while (not HasAnimSetLoaded("move_m@brave")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@brave", -1)	
-								
-		
 								end
-	
 							end
 
 							function humeurs2()
@@ -87,15 +63,9 @@ end
 								RequestAnimSet("move_m@sad@a")
 								while (not HasAnimSetLoaded("move_m@sad@a")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@sad@a", -1)	
-
-		
 								end
 							end
 
-							
-							
-	
-							
 							function humeurs4()
 								local pid = PlayerPedId()
 	
@@ -103,10 +73,7 @@ end
 								RequestAnimSet("move_m@depressed@a")
 								while (not HasAnimSetLoaded("move_m@depressed@a")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@depressed@a", -1)	
-
-		
 								end
-	
 							end
 							
 							function humeurs5()
@@ -116,13 +83,10 @@ end
 								RequestAnimSet("move_characters@michael@fire")
 								while (not HasAnimSetLoaded("move_characters@michael@fire")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_characters@michael@fire", -1)	
-
-		
 								end
 	
 							end
-							
-							
+
 							function humeurs6()
 								local pid = PlayerPedId()
 	
@@ -130,16 +94,9 @@ end
 								RequestAnimSet("move_m@quick")
 								while (not HasAnimSetLoaded("move_m@quick")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@quick", -1)	
-
-		
 								end
-	
 							end
-							
-							
-						
-							
-							
+
 							function humeurs9()
 								local pid = PlayerPedId()
 	
@@ -147,13 +104,9 @@ end
 								RequestAnimSet("move_m@confident")
 								while (not HasAnimSetLoaded("move_m@confident")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@confident", -1)	
-
-		
 								end
-	
 							end
-							
-							
+
 							function humeurs10()
 								local pid = PlayerPedId()
 	
@@ -161,13 +114,9 @@ end
 								RequestAnimSet("move_m@casual@b")
 								while (not HasAnimSetLoaded("move_m@casual@b")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@casual@b", -1)	
-
-		
 								end
-	
-							end
-							
-							
+							end	
+
 							function humeurs11()
 								local pid = PlayerPedId()
 	
@@ -175,13 +124,9 @@ end
 								RequestAnimSet("move_m@hurry@a")
 								while (not HasAnimSetLoaded("move_m@hurry@a")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@hurry@a", -1)	
-
-		
 								end
-	
 							end
-							
-							
+
 							function humeurs12()
 								local pid = PlayerPedId()
 	
@@ -189,35 +134,27 @@ end
 								RequestAnimSet("move_characters@jimmy@slow@")
 								while (not HasAnimSetLoaded("move_characters@jimmy@slow@")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_characters@jimmy@slow@", -1)	
-
-		
 								end
-	
 							end
-
-		
-							
-
 
 function demAttitudes()
     options.menu_subtitle = "Attitudes"
     ClearMenu()
-					Menu.addButton("Hautain",    						"attitude1",nil)
-					Menu.addButton("Bad Boy",             				"attitude2",nil)
-					Menu.addButton("Gangster",							"attitude3",nil)
-					Menu.addButton("Fraquassé",							"attitude4",nil)
-					Menu.addButton("Coquille vide",    					"attitude5",nil)
-					Menu.addButton("Coincé",             				"attitude6",nil)
-					Menu.addButton("Perdu",             				"attitude9",nil)
-					Menu.addButton("Intimidant",						"attitude10",nil)
-					Menu.addButton("Richissime",    					"attitude11",nil)
-					Menu.addButton("Hargneux",							"attitude12",nil)
-					Menu.addButton("Imposant",							"attitude13",nil)
-					Menu.addButton("Frimeur",			"attitude14",nil)
+					Menu.addButton("Hautain", "attitude1",nil)
+					Menu.addButton("Bad Boy", "attitude2",nil)
+					Menu.addButton("Gangster", "attitude3",nil)
+					Menu.addButton("Fraquassé", "attitude4",nil)
+					Menu.addButton("Coquille vide", "attitude5",nil)
+					Menu.addButton("Coincé", "attitude6",nil)
+					Menu.addButton("Perdu", "attitude9",nil)
+					Menu.addButton("Intimidant", "attitude10",nil)
+					Menu.addButton("Richissime", "attitude11",nil)
+					Menu.addButton("Hargneux", "attitude12",nil)
+					Menu.addButton("Imposant", "attitude13",nil)
+					Menu.addButton("Frimeur", "attitude14",nil)
 
 					Menu.addButton("~c~---------------------------", "DemarchesMenu", nil)
-					Menu.addButton("~r~Reset",								"reset", nil)
-
+					Menu.addButton("~r~Reset", "reset", nil)
 end
 
 							function attitude1()
@@ -227,10 +164,7 @@ end
 								RequestAnimSet("move_m@business@a")
 								while (not HasAnimSetLoaded("move_m@business@a")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@business@a", -1)	
-
-		
 								end
-	
 							end
 
 							function attitude2()
@@ -240,13 +174,9 @@ end
 								RequestAnimSet("move_m@casual@d")
 								while (not HasAnimSetLoaded("move_m@casual@d")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@casual@d", -1)	
-
-		
 								end
 							end
 
-							
-							
 							function attitude3()
 								local pid = PlayerPedId()
 	
@@ -254,10 +184,7 @@ end
 								RequestAnimSet("move_m@casual@e")
 								while (not HasAnimSetLoaded("move_m@casual@e")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@casual@e", -1)	
-
-		
 								end
-	
 							end
 							
 							function attitude4()
@@ -267,10 +194,7 @@ end
 								RequestAnimSet("move_m@buzzed")
 								while (not HasAnimSetLoaded("move_m@buzzed")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@buzzed", -1)	
-
-		
 								end
-	
 							end
 							
 							function attitude5()
@@ -280,10 +204,7 @@ end
 								RequestAnimSet("move_m@depressed@b")
 								while (not HasAnimSetLoaded("move_m@depressed@b")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@depressed@b", -1)	
-
-		
 								end
-	
 							end
 
 							function attitude6()
@@ -293,18 +214,8 @@ end
 								RequestAnimSet("move_m@sassy")
 								while (not HasAnimSetLoaded("move_m@sassy")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@sassy", -1)	
-
-		
 								end
 							end
-
-							
-							
-						
-					
-							
-								
-							
 
 							function attitude9()
 								local pid = PlayerPedId()
@@ -313,13 +224,9 @@ end
 								RequestAnimSet("move_m@hobo@b")
 								while (not HasAnimSetLoaded("move_m@hobo@b")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@hobo@b", -1)	
-
-		
 								end
 							end
 
-							
-							
 							function attitude10()
 								local pid = PlayerPedId()
 	
@@ -327,13 +234,9 @@ end
 								RequestAnimSet("move_m@intimidation@1h")
 								while (not HasAnimSetLoaded("move_m@intimidation@1h")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@intimidation@1h", -1)	
-
-		
 								end
-	
 							end
-						
-							
+
 							function attitude11()
 								local pid = PlayerPedId()
 	
@@ -341,16 +244,9 @@ end
 								RequestAnimSet("move_m@money")
 								while (not HasAnimSetLoaded("move_m@money")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@money", -1)	
-
-		
 								end
-	
 							end
 
-							
-
-							
-							
 							function attitude12()
 								local pid = PlayerPedId()
 	
@@ -358,10 +254,7 @@ end
 								RequestAnimSet("move_m@fire")
 								while (not HasAnimSetLoaded("move_m@fire")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@fire", -1)	
-
-		
 								end
-	
 							end
 							
 							function attitude13()
@@ -371,10 +264,7 @@ end
 								RequestAnimSet("move_m@gangster@generic")
 								while (not HasAnimSetLoaded("move_m@gangster@generic")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@gangster@generic", -1)	
-
-		
 								end
-	
 							end
 							
 								function attitude14()
@@ -384,29 +274,21 @@ end
 								RequestAnimSet("move_m@swagger")
 								while (not HasAnimSetLoaded("move_m@swagger")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@swagger", -1)	
-
-		
 								end
-	
 							end
-							
-							
 
 function demAutres()
     options.menu_subtitle = "Autres"
     ClearMenu()
-					Menu.addButton("Randonneur",		"autres1",nil)
-					Menu.addButton("Blessé",    		"autres2",nil)
-					Menu.addButton("Obèse",				"autres3",nil)
-					Menu.addButton("Mal aux fesses", 	"autres4",nil)
-					Menu.addButton("Detente",		"autres6",nil)
-
+					Menu.addButton("Randonneur", "autres1",nil)
+					Menu.addButton("Blessé", "autres2",nil)
+					Menu.addButton("Obèse",	"autres3",nil)
+					Menu.addButton("Mal aux fesses", "autres4",nil)
+					Menu.addButton("Detente", "autres6",nil)
 
 					Menu.addButton("~c~---------------------------", "DemarchesMenu", nil)
-					Menu.addButton("~r~Reset",							"reset", nil)
-
+					Menu.addButton("~r~Reset", "reset", nil)
 end
-
 
 							function autres1()
 								local pid = PlayerPedId()
@@ -415,10 +297,7 @@ end
 								RequestAnimSet("move_m@hiking")
 								while (not HasAnimSetLoaded("move_m@hiking")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@hiking", -1)	
-
-		
 								end
-	
 							end
 
 							function autres2()
@@ -428,12 +307,8 @@ end
 								RequestAnimSet("move_m@injured")
 								while (not HasAnimSetLoaded("move_m@injured")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@injured", -1)	
-
-		
 								end
 							end
-
-							
 							
 							function autres3()
 								local pid = PlayerPedId()
@@ -442,10 +317,7 @@ end
 								RequestAnimSet("move_m@fat@a")
 								while (not HasAnimSetLoaded("move_m@fat@a")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@fat@a", -1)	
-
-		
 								end
-	
 							end
 							
 							function autres4()
@@ -460,10 +332,7 @@ end
 								end
 	
 							end
-							
-							
-						
-							
+
 							function autres6()
 								local pid = PlayerPedId()
 	
@@ -471,29 +340,25 @@ end
 								RequestAnimSet("move_m@swagger@b")
 								while (not HasAnimSetLoaded("move_m@swagger@b")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_m@swagger@b", -1)	
-
-		
 								end
-	
 							end
 							
-							function demFemmes()
+function demFemmes()
     options.menu_subtitle = "Special Femmes"
     ClearMenu()
-					Menu.addButton('Arrogante',    						"femmes1",nil)
-					Menu.addButton('Classique',							"femmes3",nil)
-					Menu.addButton('Fragile',             			"femmes5",nil)
-					Menu.addButton('Enervée',							"femmes6",nil)
-					Menu.addButton('Femme fatale',             				"femmes8",nil)
-					Menu.addButton('Fuite',    							"femmes10",nil)
-					Menu.addButton('Tristesse',             			"femmes11",nil)
-					Menu.addButton('Rebelle',							"femmes12",nil)
-					Menu.addButton('Serieuse',    							"femmes13",nil)
-					Menu.addButton('Roule des fesses',             				"femmes14",nil)
-					Menu.addButton('Hautaine',							"femmes15",nil)
+					Menu.addButton('Arrogante', "femmes1",nil)
+					Menu.addButton('Classique', "femmes3",nil)
+					Menu.addButton('Fragile', "femmes5",nil)
+					Menu.addButton('Enervée', "femmes6",nil)
+					Menu.addButton('Femme fatale', "femmes8",nil)
+					Menu.addButton('Fuite', "femmes10",nil)
+					Menu.addButton('Tristesse', "femmes11",nil)
+					Menu.addButton('Rebelle', "femmes12",nil)
+					Menu.addButton('Serieuse', "femmes13",nil)
+					Menu.addButton('Roule des fesses', "femmes14",nil)
+					Menu.addButton('Hautaine', "femmes15",nil)
 					Menu.addButton("~c~---------------------------", "DemarchesMenu", nil)
-					Menu.addButton('~r~Reset',							"reset", nil)
-
+					Menu.addButton('~r~Reset', "reset", nil)
 end
 
 							function femmes1()
@@ -503,15 +368,9 @@ end
 								RequestAnimSet("move_f@arrogant@a")
 								while (not HasAnimSetLoaded("move_f@arrogant@a")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_f@arrogant@a", -1)	
-
-			
 								end
-	
 							end
 
-							
-							
-							
 							function femmes3()
 								local pid = PlayerPedId()
 	
@@ -519,13 +378,9 @@ end
 								RequestAnimSet("move_characters@amanda@bag")
 								while (not HasAnimSetLoaded("move_characters@amanda@bag")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_characters@amanda@bag", -1)	
-
-		
 								end
-	
 							end
-							
-							
+
 							function femmes5()
 								local pid = PlayerPedId()
 	
@@ -533,13 +388,9 @@ end
 								RequestAnimSet("move_f@femme@")
 								while (not HasAnimSetLoaded("move_f@femme@")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_f@femme@", -1)	
-
-		
 								end
-	
 							end
-							
-							
+
 							function femmes6()
 								local pid = PlayerPedId()
 	
@@ -547,15 +398,8 @@ end
 								RequestAnimSet("move_f@gangster@ng")
 								while (not HasAnimSetLoaded("move_f@gangster@ng")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_f@gangster@ng", -1)	
-
-		
 								end
-	
 							end
-							
-						
-							
-							
 							
 							function femmes8()
 								local pid = PlayerPedId()
@@ -564,16 +408,9 @@ end
 								RequestAnimSet("move_f@heels@c")
 								while (not HasAnimSetLoaded("move_f@heels@c")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_f@heels@c", -1)	
-
-		
 								end
-	
 							end
-							
-							
-							
-							
-							
+
 							function femmes10()
 								local pid = PlayerPedId()
 	
@@ -581,13 +418,9 @@ end
 								RequestAnimSet("move_f@hurry@b")
 								while (not HasAnimSetLoaded("move_f@hurry@b")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_f@hurry@b", -1)	
-
-		
 								end
-	
 							end
-							
-							
+
 							function femmes11()
 								local pid = PlayerPedId()
 	
@@ -595,13 +428,9 @@ end
 								RequestAnimSet("move_f@sad@a")
 								while (not HasAnimSetLoaded("move_f@sad@a")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_f@sad@a", -1)	
-
-		
 								end
-	
 							end
-							
-							
+
 							function femmes12()
 								local pid = PlayerPedId()
 	
@@ -609,10 +438,7 @@ end
 								RequestAnimSet("move_f@sassy")
 								while (not HasAnimSetLoaded("move_f@sassy")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_f@sassy", -1)	
-
-		
 								end
-	
 							end
 							
 							function femmes13()
@@ -622,13 +448,8 @@ end
 								RequestAnimSet("move_f@sexy@a")
 								while (not HasAnimSetLoaded("move_f@sexy@a")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_f@sexy@a", -1)	
-
-		
 								end
-	
 							end
-							
-							
 							function femmes14()
 								local pid = PlayerPedId()
 	
@@ -636,13 +457,9 @@ end
 								RequestAnimSet("move_f@tough_guy@")
 								while (not HasAnimSetLoaded("move_f@tough_guy@")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_f@tough_guy@", -1)	
-
-		
 								end
-	
 							end
-							
-							
+
 							function femmes15()
 								local pid = PlayerPedId()
 	
@@ -650,17 +467,8 @@ end
 								RequestAnimSet("move_f@tool_belt@a")
 								while (not HasAnimSetLoaded("move_f@tool_belt@a")) do Citizen.Wait(0) end
 								SetPedMovementClipset(GetPlayerPed(-1), "move_f@tool_belt@a", -1)	
-
-		
 								end
-	
 							end
-
-
-
-		  	
-	
-
 
 function reset()
 local pid = PlayerPedId()
@@ -672,18 +480,14 @@ function quitter ()
 Menu.hidden = true
 end
 
-
-
 -- Key Controls
-
-
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-		if IsControlJustPressed(1, Keys["BACKSPACE"]) then -- Si touche retour on cache
+		if IsControlJustPressed(1, 177) then -- Si touche retour on cache
 			Menu.hidden = true
 		end
-        if IsControlJustPressed(1, Keys["F10"]) then --Modifier L'ouverture du Menu Ici ! Touche F3 de base.
+        if IsControlJustPressed(1, 57) then --Modifier L'ouverture du Menu Ici ! Touche F3 de base.
             DemarchesMenu()
 			-- Menu to draw
             Menu.hidden = not Menu.hidden -- Hide/Show the menu
