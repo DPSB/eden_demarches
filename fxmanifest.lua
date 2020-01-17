d42346b76c790c25bef19c491602742341724d31
rex2630 <rex2630@gmail.com>
2020-01-17 22:46:22 +0100

    Replace __resource.lua with fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Eden Demarches'
+
+client_scripts {
+    '@es_extended/locale.lua',
+    'locales/en.lua',
+    'locales/fr.lua',
+    'config.lua',
+    'client/main.lua',
+    'GUI.lua'
+}
